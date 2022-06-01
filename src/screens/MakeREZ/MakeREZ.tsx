84d6de912b4c42ad6d627091fcc8486b2e4ac67f
Young <willy4202@naver.com>
2022-06-01 22:58:02 +0900

    Modify: import 라인 삭제, 및 경로 수정

--- src/screens/MakeREZ/MakeREZ.tsx
+++ src/screens/MakeREZ/MakeREZ.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {HomeStackParamList} from '~/../App';

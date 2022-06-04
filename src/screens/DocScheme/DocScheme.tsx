e8952035e295d2f3935abcd4f8a7b0c2d17afbfc
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2022-06-04 22:49:32 +0900

    메인작업 완료

--- src/screens/DocScheme/DocScheme.tsx
+++ src/screens/DocScheme/DocScheme.tsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, Image, Pressable} from 'react-native';
 import styled, {css} from 'styled-components/native';
 import {NewDate} from '~/src/types/type';
 import {DocSchemeNavigationProps} from '~/src/types/type';

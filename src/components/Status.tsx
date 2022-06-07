739f4fc038dd7545809ef65f8c419d33e43db971
Young <willy4202@naver.com>
2022-06-07 11:33:13 +0900

    Add: status 컴포넌트화 진행 중

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
 
 function Status() {
   return (
     <View>
-      <Text>Main</Text>
+      <StatusTitle>{status}</StatusTitle>
     </View>
   );
 }
 
 export default Status;
+const StatusTitle = styled.Text`
+  background-color: yellow;
+  padding: 2px 10px;
+`;

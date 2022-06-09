307d09eb9b69d0dec3661ee717df5d0397456c58
dogsocks <dogsocks@dogsockcBookPro.lan>
2022-06-09 10:34:10 +0900

    parent ab2c0b0575699f3e906056f31bd3f7fbec9e6066 author dogsocks <dogsocks@dogsockcBookPro.lan> 1654738450 +0900 committer dogsocks <dogsocks@dogsockcBookPro.lan> 1654747200 +0900
    리뷰반영및 컨플릭트해결

--- src/screens/DocList/DocList.tsx
+++ src/screens/DocList/DocList.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Dimensions, FlatList} from 'react-native';
 import styled from 'styled-components/native';
 import {DocListNavigationProps, InitialDocListProp} from '~/src/types/type';
 import DoctorCard from '@components/DoctorCard';
+import {DocInfoContext} from '~/src/ReservationContext';
 
 const DATA: InitialDocListProp[] = [];
 for (let i = 0; i < 100; i++) {
@@ -28,16 +29,19 @@ function DocList({navigation, route}: DocListNavigationProps) {
     }),
   );
 
+  const {setDocInfo} = useContext(DocInfoContext);
+
   const [initialDocData, setInitialDocData] = useState(
     DATA.slice(0, renderItemNum),
   );
 
-  const goDocScheme = () => {
+  const goDocScheme = (docInfo: InitialDocListProp) => {
+    setDocInfo(docInfo);
     navigation.navigate('DocScheme');
   };
 
   const renderItem = ({item}: {item: InitialDocListProp}) => (
-    <ListButton onPress={goDocScheme}>
+    <ListButton onPress={() => goDocScheme(item)}>
       <DoctorCard docData={item}></DoctorCard>
     </ListButton>
   );
--- src/screens/REZList/REZList.tsx
+++ src/screens/REZList/REZList.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {Dimensions, FlatList} from 'react-native';
 import styled from 'styled-components/native';
 import {REZListNavigationProps, InitialDocListProp} from '~/src/types/type';
 import DoctorCard from '@components/DoctorCard';
 import CalendarImage from '@assets/images/calendar_icon.png';
+import {DocInfoContext} from '~/src/ReservationContext';
 
 const DATA: InitialDocListProp[] = [];
 for (let i = 0; i < 100; i++) {
@@ -26,16 +27,19 @@ function REZList({navigation}: REZListNavigationProps) {
     DATA.slice(0, renderItemNum),
   );
 
+  const {setDocInfo} = useContext(DocInfoContext);
+
   useEffect(() => {
     navigation.setOptions({title: '예약 목록', headerShadowVisible: false});
   });
 
-  const goDocScheme = () => {
+  const goDocScheme = (docInfo: InitialDocListProp) => {
+    setDocInfo(docInfo);
     navigation.navigate('REZDetail');
   };
 
   const renderItem = ({item}: {item: InitialDocListProp}) => (
-    <ListButton onPress={goDocScheme}>
+    <ListButton onPress={() => goDocScheme(item)}>
       <ListHeader>
         <DateWrapper>
           <CalendarIcon source={CalendarImage} resizeMode="contain" />
